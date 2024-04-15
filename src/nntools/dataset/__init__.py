3d8052cc7d76f7b07b7e1a9b2d93ca2bad4830d3
ClementPla <clement.playout@polymtl.ca>
2024-04-15 11:33:36 -0400

    Fix the field for cache option

--- src/nntools/dataset/__init__.py
+++ src/nntools/dataset/__init__.py
@@ -2,7 +2,8 @@ from nntools.dataset.classif_dataset import ClassificationDataset
 from nntools.dataset.composer import Composition, nntools_wrapper
 from nntools.dataset.image_dataset import ImageDataset, MultiImageDataset
 from nntools.dataset.seg_dataset import SegmentationDataset
-from nntools.dataset.utils import class_weighting, get_segmentation_class_count, random_split
+from nntools.dataset.utils.balance import class_weighting, get_segmentation_class_count
+from nntools.dataset.utils.ops import random_split
 
 __all__ = [
     "ClassificationDataset",
